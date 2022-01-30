a30f266737941cd5d068685302b08bb9e93ec26f
RBhai <rohanchander@Rohans-MacBook-Pro.local>
2022-01-30 03:26:44 -0500

    dub

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let map;
 //var bounds = new google.maps.LatLngBounds();
-
+const UF_Bounds = {
+  north:29.652127882560325,
+  south:29.63041832222747,
+  east:-82.3393812215348,
+  west:-82.37254046414034,
+};
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+
+    map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 29.6516344, lng: -82.3248262 },
+    restriction:{
+      latLngBounds: UF_Bounds,
+      strictBounds: false,
+    },
     zoom: 12,
     mapId: 'fdfa1f14231eb459'
   });
