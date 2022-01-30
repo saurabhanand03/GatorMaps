21c9ecf5acf42f671a99184779f380acf1ba53e8
Michael <michael.milligan@ufl.edu>
2022-01-30 00:34:41 -0500

    trying to test marker . 1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let map;
-var bounds = new google.maps.LatLngBounds();
+//var bounds = new google.maps.LatLngBounds();
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -8,3 +8,14 @@ function initMap() {
     mapId: 'fdfa1f14231eb459'
   });
 }
+
+//var fs = require('fs');
+
+// Use fs.readFile() method to read the file
+fs.readFile('DiningHours.txt', 'utf8', function(err, data){
+
+  // Display the file content
+  console.log(data);
+});
+
+console.log('readFile called');
