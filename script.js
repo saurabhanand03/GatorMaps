723030e78b35428400f3d7868c39c34a59f9aa5d
Michael <michael.milligan@ufl.edu>
2022-01-30 04:26:31 -0500

    Add window function

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const UF_Bounds = {
   east:-82.3393812215348,
   west:-82.37254046414034,
 };
-function initMap() {
 
+function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 29.6516344, lng: -82.3248262 },
     restriction:{
@@ -18,10 +18,20 @@ function initMap() {
     mapId: 'fdfa1f14231eb459'
   });
 
-  const beachMarker = new google.maps.Marker({
+  const testMarker1 = new google.maps.Marker({
     position: { lat: 29.646126638775236, lng: -82.34788034625916},
     map,
     icon: "Orange_Marker.png",
+  });
+  const testWindow1 = new google.maps.InfoWindow({
+      content: "test",
+  });
+  testMarker1.addListener("click", () => {
+      testWindow1.open({
+          anchor: testMarker1,
+          map,
+          shouldFocus: false,
+      });
   });
 
 }
